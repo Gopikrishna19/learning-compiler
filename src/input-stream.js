d2d69af5663d9dba759b113add6d9b4fbe239ec9
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-23 19:12:11 -0500

    read next character from the stream

--- src/input-stream.js
+++ src/input-stream.js
@@ -1,10 +1,32 @@
 class InputStream {
 
-  constructor() {
+  constructor(input) {
 
     this.column = 0;
-    this.line = 1;
+    this.input = input;
     this.position = 0;
+    this.row = 1;
+
+  }
+
+  next() {
+
+    const char = this.input.charAt(this.position);
+
+    if (char === '\n') {
+
+      this.row += 1;
+      this.column = 0;
+
+    } else {
+
+      this.column += 1;
+
+    }
+
+    this.position += 1;
+
+    return char;
 
   }
 
--- test/input-stream.js
+++ test/input-stream.js
@@ -3,22 +3,106 @@ const {InputStream} = require('../src/input-stream');
 
 describe('Input Stream', () => {
 
-  let stream;
+  describe('initialization', () => {
 
-  beforeEach(() => stream = new InputStream());
+    let stream;
 
-  it('should be initialized and used as an object', () => {
+    beforeEach(() => stream = new InputStream());
 
-    expect(stream).object();
+    it('should be initialized and used as an object', () => {
+
+      expect(stream).object();
+
+    });
+
+    it('should have initial configurations', () => {
+
+      expect(stream).includes({
+        column: 0,
+        position: 0,
+        row: 1
+      });
+
+    });
 
   });
 
-  it('should have initial configurations', () => {
+  describe('when reading next character', () => {
 
-    expect(stream).includes({
-      column: 0,
-      line: 1,
-      position: 0
+    describe('if next character is newline', () => {
+
+      let nextChar,
+        stream;
+
+      beforeEach(() => {
+
+        stream = new InputStream('\n');
+        nextChar = stream.next();
+
+      });
+
+      it('should increment the row', () => {
+
+        expect(stream.row).to.equal(2);
+
+      });
+
+      it('should reset the column', () => {
+
+        expect(stream.column).to.equal(0);
+
+      });
+
+      it('should increment the position', () => {
+
+        expect(stream.position).to.equal(1);
+
+      });
+
+      it('should return the next character', () => {
+
+        expect(nextChar).to.equal('\n');
+
+      });
+
+    });
+
+    describe('if next character is not a newline', () => {
+
+      let nextChar,
+        stream;
+
+      beforeEach(() => {
+
+        stream = new InputStream('a');
+        nextChar = stream.next();
+
+      });
+
+      it('should stay in the same the row', () => {
+
+        expect(stream.row).to.equal(1);
+
+      });
+
+      it('should increment the column', () => {
+
+        expect(stream.column).to.equal(1);
+
+      });
+
+      it('should increment the position', () => {
+
+        expect(stream.position).to.equal(1);
+
+      });
+
+      it('should return the next character', () => {
+
+        expect(nextChar).to.equal('a');
+
+      });
+
     });
 
   });
