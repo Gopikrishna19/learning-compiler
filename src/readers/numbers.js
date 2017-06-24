b9cc85f66d8368738d93e9ee608d24b00e81abf7
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-24 12:19:10 -0500

    use read while

--- src/readers/numbers.js
+++ src/readers/numbers.js
@@ -1,32 +1,16 @@
 const {isOneOf} = require('./utils/is.js');
+const {readWhile} = require('./utils/while.js');
 
 const DECIMALS = new Set(['.']);
 const NUMBER = Symbol.for('NUMBER');
 
 const isNumber = char => char >= '0' && char <= '9';
+const isValidNumber = char => isNumber(char) || isOneOf(char, DECIMALS);
 
-const readNumber = input => {
-
-  let sequence = '';
-
-  while (!input.EOF) {
-
-    const char = input.next();
-
-    if (isNumber(char) || isOneOf(char, DECIMALS)) {
-
-      sequence += char;
-
-    }
-
-  }
-
-  return {
-    type: NUMBER,
-    value: Number(sequence)
-  };
-
-};
+const readNumber = input => ({
+  type: NUMBER,
+  value: Number(readWhile(input, isValidNumber))
+});
 
 module.exports = {
   isNumber,
