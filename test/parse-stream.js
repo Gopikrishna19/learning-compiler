4fcd99248fd0983230b2ca70dcfd1664f490d753
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-07-09 22:15:15 -0500

    try to parse full program without error

--- test/parse-stream.js
+++ test/parse-stream.js
@@ -624,6 +624,33 @@ describe('Parse Stream', () => {
 
     });
 
+    it('should parse a full program without error', () => {
+
+      expect(() => parse(`
+        # this is a comment
+      
+        println("Hello World!");
+        
+        println(2 + 3 * 4);
+        
+        # functions are introduced with "lambda"
+        fib = lambda (n) if n < 2 then n else fib(n - 1) + fib(n - 2);
+        
+        println(fib(15));
+        
+        print-range = λ(a, b)             
+                        if a <= b then {  # "then" here is optional as you can see below
+                          print(a);
+                          if a + 1 <= b {
+                            print(", ");
+                            print-range(a + 1, b);
+                          } else println("");        # newline
+                        };
+        print-range(1, 5);
+      `)).to.not.throw();
+
+    });
+
   });
 
 });
